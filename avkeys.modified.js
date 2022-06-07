8f1f83a37f253ef60298784275dd319f0fe58639
debil03311 <andrei.hent@yahoo.com>
2022-06-07 13:33:24 +0100

    Good lord

--- avkeys.modified.js
+++ avkeys.modified.js
@@ -27,9 +27,13 @@ function createKey(username) {
   }
 
   numberString0 += EndBytes[Math.floor(Math.random() * EndBytes.length)].toString();
+  counter++;
   let numberString1 = numberString0;
 
-  while (counter != 15) {
+  if (counter > 15)
+    counter = 15;
+
+  while (counter < 15) {
     numberString1 += (10 + Math.floor(Math.random() * 89)).toString();
     counter++;
   }
